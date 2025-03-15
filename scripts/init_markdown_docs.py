cb0b538bf17d60bfa21cb19f586dab95dd0e1c64
blutterfly <137254356+blutterfly@users.noreply.github.com>
2025-03-15 17:35:35 -0400

    Create init_markdown_docs.py

---
+++ scripts/init_markdown_docs.py
@@ -0,0 +1,5 @@
+"""
+Purpose:
+1. Use mkdocs.yml nav section to create directories and create corresponding md files
+2. Add content to these files which contains Title derived from the name of the md file and brief description of the md content.
+"""
